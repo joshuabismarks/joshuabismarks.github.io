3332d52cef90daffa7d74abec449d12ccf591f0c
Kyle-Griffin <KyleFileGriffin@gmail.com>
2019-11-13 20:27:52 +0000

    V1.02
    Fixed further issues on masonry

--- bismarks.js
+++ bismarks.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', (event) => {
 
+    var grid = document.querySelector('.projects-grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
   });
